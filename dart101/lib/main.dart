3e1eccfedb93484843061f692ef3cd99736781ab
Melik Alihocagil <79751540+melikalihocagil@users.noreply.github.com>
2022-12-15 14:08:29 +0300

    veri tipleri eklendi

---
+++ dart101/lib/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  // const DART101 = "I LOVE DART";
+  // print(DART101);
+  // final DART_FINAL = "THIS IS FINAL VALUE";
+  // print(DART_FINAL);
+  String name = "Dart";
+  String name2 = "\nI love to develop applications with \nFlutter";
+  //  String name2 = r"\nI love to develop applications with \nFlutter";
+  //olsaydı \n newline olmaktan çıkıp aynı şekilde yazdırılırdı metin.
+  String name3 = "The price is \$10";
+  //$ olsaydı hata alırdık \$ olarak kullanılması gerekli.
+  print(name + " " + name2);
+  print(name3);
+}
---
+++ veri_tipleri/android/app/src/main/kotlin/com/example/veri_tipleri/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.veri_tipleri
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
